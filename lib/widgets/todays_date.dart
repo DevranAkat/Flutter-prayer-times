4d1f8267d00b9c49daf9e092eba7e0d9a11f936c
DevranAkat <devran.akat@hotmail.com>
2021-12-09 23:37:05 +0100

    Added todays date to TimeContainer

--- lib/widgets/todays_date.dart
+++ lib/widgets/todays_date.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' show DateFormat;
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:hijri/hijri_calendar.dart';
 
 class DateContainer extends StatelessWidget {
-  final String date = "Çarşamba 7 Aralık 2021";
-  final String dateHicri = "13 Rebîulâhir 1443";
   
   const DateContainer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //Remove these? 
+    initializeDateFormatting('tr_TR', null);
+    initializeDateFormatting('ar', null);
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.symmetric(vertical: 17.0),
       decoration: BoxDecoration(color: Color(0xff222831), borderRadius: BorderRadius.all(Radius.circular(30))),
       child: Column(
         children: [
-          Text(date, style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
+          Text(todaysDateTurkish(), style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
           Padding(
             padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
-            child: Text(dateHicri,
-              style: TextStyle(color: Colors.white))),
+            child: Text(todaysDateHijri(), style: TextStyle(color: Colors.white))),
         ],
       ),
     );
   }
 }
 
-String getTodaysDate() {
+String todaysDateTurkish() {
   DateTime now =  DateTime.now();
-  DateTime date =  DateTime(now.year, now.month, now.day); 
-  return date.toString();
+  return DateFormat('EEEE, d MMMM yyyy', 'tr_TR').format(now); 
 }
+
+String todaysDateHijri() => HijriCalendar.now().toFormat("dd MMMM yyyy");
